bc4a2f68bfa2cecf34808aaca90f0f4469a6c99b
Reihan Fahlevi <whenautumnend@gmail.com>
2025-09-25 15:01:46 +0700

    feat: deck&flashcard model & table

---
+++ app/Models/Deck.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Deck extends Model
+{
+    use SoftDeletes;
+
+    protected $fillable = [
+        'title',
+        'description',
+        'is_public',
+        'user_id',
+
+        'created_by',
+        'updated_by',
+        'deleted_by',
+        'created_at',
+        'updated_at',
+        'deleted_at'
+    ];
+
+    public function user(): BelongsTo {
+        return $this->belongsTo(User::class);
+    }
+
+    public function flashcards(): HasMany {
+        return $this->hasMany(Flashcard::class);
+    }
+}
---
+++ app/Models/Flashcard.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Flashcard extends Model
+{
+    use SoftDeletes;
+
+    protected $fillable = [
+        'question',
+        'answer',
+        'deck_id',
+
+        'created_by',
+        'updated_by',
+        'deleted_by',
+        'created_at',
+        'updated_at',
+        'deleted_at'
+    ];
+
+    public function deck(): BelongsTo {
+        return $this->belongsTo(Deck::class);
+    }
+}
