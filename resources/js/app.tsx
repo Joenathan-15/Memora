e563e89aebd263bc9d2f6222764d8872ab652c3a
Reihan Fahlevi <whenautumnend@gmail.com>
2025-09-28 19:59:02 +0700

    feat: dropzone css

--- resources/js/app.tsx
+++ resources/js/app.tsx
@@ -1,5 +1,6 @@
 import '../css/app.css';
 import '@mantine/core/styles.css';
+import '@mantine/dropzone/styles.css';
 
 import { createInertiaApp } from '@inertiajs/react';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
